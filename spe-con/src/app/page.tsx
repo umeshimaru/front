80ead61c288e4f6b8b913b8a0082d7cfd1311722
umekishimaru <riki12344649@gmail.com>
2025-05-29 13:48:44 +0900

    feature/signup ユーザー登録機能実装

--- spe-con/src/app/page.tsx
+++ spe-con/src/app/page.tsx
@@ -1,13 +1,17 @@
+"use client"
 import { Header } from "./common/components/Header";
-import { Home } from "./top-page/Home";
+import { Home } from "./home/Home";
+
 
 export default function Page() {
   return (
     <>
+        
       <Header />
       <main>
         <Home />
       </main>
+      
     </>
   );
 }
---
+++ spe-con/src/app/practice-menu/page.tsx
@@ -0,0 +1,11 @@
+import { PracticeMenu } from "@/app/practice-menu/components/practice-menu"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 py-4 sm:py-6 md:py-8 lg:py-12">
+      <div className="container mx-auto px-2 sm:px-4 md:px-6 lg:px-8">
+        <PracticeMenu />
+      </div>
+    </main>
+  )
+}
